211fe066d38315e871f8962fe712f01616c5268d
LebedenkoAndrii <lebedenko1928@gmail.com>
2023-11-17 23:23:26 +0200

    added experience section, fixed many bugs with burger menu button, addaptived for tablet pc and smartphones

--- src/script.js
+++ src/script.js
@@ -2,6 +2,17 @@ $(".menu-btn").on("click", function (e) {
   e.preventDefault();
   $(".menu").toggleClass("menu-active");
   $(".menu-btn").toggleClass("active");
+  $("body").toggleClass("lock");
   $(".main").toggleClass("main-active");
 });
+// scroll-bar
+function scrollBarWidth() {
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height =
+    document.body.scrollHeight - document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
+  document.getElementById("bar").style.width = scrolled + "%";
+}
+window.onscroll = scrollBarWidth;
+
 AOS.init();
